09bb83191ccbe689af4f2465137272dbe04032d6
Carlos Pumar <carlospumar@carloss-mbp.localdomain>
2024-02-24 20:25:36 +0100

    refactor class instatiation

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-viz = Visualizer()
-
 url = 'https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv'
 
 @st.cache_data
