6d185de22c21fc5a52722375e8c3e9d61b37de25
Carlos Pumar <cpumarfrohberg@gmail.com>
2024-02-27 12:25:18 +0100

    update interprepation of plots

--- app.py
+++ app.py
@@ -38,8 +38,11 @@ if nav == 'Home':
         info_str = buffer.getvalue()
         st.text(info_str)
 
-    multi = '''We see that only one variable is categorical/nominal (`model`), all other ones are numeric data.  
-    Of these latter, `mpg`, `disp`, `drat`, `wt`, and `qsec` are continuous. The rest are integer variables.'''
+    multi = '''
+    We see that only one variable is categorical/nominal (`model`), all other ones are numeric data.  
+    Of these latter, `mpg`, `disp`, `drat`, `wt`, and `qsec` are continuous. The rest are integer variables.
+    No missing data.
+    '''
     st.markdown(multi)
     
 if nav == 'Numeric Variables':
@@ -48,31 +51,32 @@ if nav == 'Numeric Variables':
         st.table(cars.describe())
     
         first_working_hypo = '''
-        * First working hypothesis: **variables are normally distributed**. However:
-            - comparing mean and median values, we note that this might not be the case (e.g., `disp`, but also `hp`...and other ones - can you spot which ones?)
-            - in fact, by comparing mean and median values, we can already say that either the data-generating processes (DGP) are non-uniform or that we have outliers.
+        * First working hypothesis: **variables are normally distributed**. 
+            - However, when comparing mean and median values in the summary stats, we note that this might not be the case (e.g., `disp`, but also `hp`...and other ones - can you spot which ones?)
+            - in fact, by comparing named values with each other, we expect that either the data-generating process (DGP) is non-normal or that we have outliers.
             - in addition, and by comparing the first and third quartiles with the median, we can check if our variables are left (`disp`) or right-skewed (e.g., `hp`)
-            - **we will use plots to see if our hypotheses holds**
+            - **we will use plots to see if our hypotheses hold**
         '''
         st.markdown(first_working_hypo)
         
         second_working_hypo = '''
         * Second working hypothesis: **some variables are correlated**:
-            - we would expect some variables to be correlated with each other, e.g., horsepower (i.e.,  
-            `hp` and mileage per gallon (`mpg`) to be positively correlated)
-            - **we will use plots to see if our hypotheses holds**
+            - we would expect some variables to be correlated one with another, e.g. horsepower (`hp`) and mileage per gallon (`mpg`) (expectation: positive corr)
+        * **we will use plots to see if our hypotheses hold**
         '''
         st.markdown(second_working_hypo)
 
-    if st.checkbox("<- Let's check box plots"):
+    if st.checkbox("<- Click here in order to check box plots"):
         boxplots = car_viz.boxplot(columns=numeric.columns, y_max=None)
         st.pyplot(boxplots)
 
         interpret_boxplots = '''
-        * Re first working hypothesis: **only one variable seems to be normally distributed:`cyl`**
-            - boxplots show that all other variables are either left or right-skewed
-            - also, we see that for `mpg`, `hp`, `qsec`, `carb` and `wt`, we either might have outliers or we are looking into a non-normal DGP
-            - we will **need to use additional plots and/or run statistical tests in order to see if our hypotheses could be true**
+        * Re first working hypothesis: 
+            - ** only *one* variable seems to be normally distributed: `cyl`**
+            - also, all other variables are either left or right-skewed
+            - additionally, we see that for `mpg`, `hp`, `qsec`, `carb` and `wt`, we either might have outliers or we are looking into a non-normal DGP
+                - this indicates we should expand our analysis to an analysis of correlation (since we could simply have non-normal distributions and "outliers" actually are not outliers, but simply follow a non-normal distribution)
+            - we will **need to use additional plots and/or run statistical tests in order to see if our hypotheses actually hold**
         '''
         st.markdown(interpret_boxplots)
 
@@ -82,9 +86,8 @@ if nav == 'Numeric Variables':
     
         interpret_pairplot = '''
         * Re first working hypothesis: **we cannot be sure that any of the variables are normally distributed**
-            - in addition, we can see that some variables seem to be correlated one with the other; hence, it would be interesting to additionally plot the degree of correlation between selected pairs of vars
-            - **we will need to user additional plots and/or run statistical tests in order to see if our second  
-            hypothesis holds**
+            - in addition, we can see that some variables seem to be correlated one with the other
+            - **we will need to user additional plots and/or run statistical tests in order to see if our second hypothesis holds**
         '''
         st.markdown(interpret_pairplot)
 
@@ -96,7 +99,7 @@ if nav == 'Numeric Variables':
         interpret_qqplot = '''
         * Re first working hypothesis: **we cannot be sure that any of the variables are normally distributed**
             - rather, we see that only `mpg` seems to comply to this assumption (since most of the datapoints lie on the line of of the theoretical quantiles)
-            - at the same time, the datapoints which are relatively far from the line indicate either the existence of tails or of outliers, which requires additional analysis
+            - at the same time, the datapoints which are relatively far from the line indicate either the existence of tails or of outliers
             - finally, we see that the integer variables (e.g. `cyl`) require additional tests in order to check for normality (horizontal lines are due to duplicated values, which result from identical decimal values)
             - we will need to **use additional plots and/or run statistical tests in order to see if our second  
             hypothesis holds**
